79a26e74a987f42890753c6356532fd2ce918a0a
Sudip Awasthi <dipsu1873@gmail.com>
2024-05-31 19:29:21 +0545

    Silder baneyoo

--- Bhancha Ghar/assets/js/index.js
+++ Bhancha Ghar/assets/js/index.js
@@ -44,3 +44,20 @@ draggables.forEach(draggable => {
     document.addEventListener('mouseup', onMouseUp);
   }
 });
+
+//Slider
+
+const swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',  //slide
+    loop: true,  //eslai false ma rakda herneyy ok
+    mousewheel: {
+      invert: false, 
+    },
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+  
+  
