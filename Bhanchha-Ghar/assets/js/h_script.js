50edca80c083aebaa6c8814c81dc852fb1ed45e7
Sudip Awasthi <dipsu1873@gmail.com>
2024-06-13 17:41:38 +0545

    Add files via upload

---
+++ Bhanchha-Ghar/assets/js/h_script.js
@@ -0,0 +1,31 @@
+let profileDropdownList = document.querySelector(".profile-dropdown-list");
+let btn = document.querySelector(".profile-dropdown-btn");
+
+let classList = profileDropdownList.classList;
+
+const toggle = () => classList.toggle("active");
+
+window.addEventListener("click", function (e) {
+  if (!btn.contains(e.target)) classList.remove("active");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('.navbar a[href^="#"]');
+  
+    for (const link of links) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+  
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+  
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+          });
+        }
+      });
+    }
+  });
+  
