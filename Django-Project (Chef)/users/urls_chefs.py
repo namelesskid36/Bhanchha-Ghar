a1f0f5eead16875fa25554a8b436e19056365837
Sudip Awasthi <dipsu1873@gmail.com>
2024-06-28 06:15:38 +0545

    Delete Django-Project (Chef) directory

--- Django-Project (Chef)/users/urls_chefs.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ChefRegisterView, ChefLoginView, chef_profile, chef_dashboard
-
-urlpatterns = [
-    path('register/', ChefRegisterView.as_view(), name='chef-register'),
-    path('login/', ChefLoginView.as_view(), name='chef-login'),
-    path('profile/', chef_profile, name='chef-profile'),
-    path('dashboard/', chef_dashboard, name='chef-dashboard'),
-]
