006ff95b760dc0de4a4e7c4797111b3d58690688
Sudip Awasthi <dipsu1873@gmail.com>
2024-07-24 12:47:44 +0545

    SWAHAAAA

--- BhanchaGhar (Django)/job_platform/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import JobPost, Application, Profile
-
-admin.site.register(JobPost)
-admin.site.register(Application)
-admin.site.register(Profile)
--- BhanchaGhar (Django)/job_platform/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobPlatformConfig(AppConfig):
-    name = 'job_platform'
